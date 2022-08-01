aa2ea871ff6c30b86a0d12fe2b83085e6f4524e6
Andy Weber <lolhiguys1@gmail.com>
2022-08-01 01:37:17 +0000

    remove *.example.* from being copied to active/archive folders

--- src/promote-draft.sh
+++ src/promote-draft.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo here
-
 dat=$(date +'%m.%d.%y-%H:%M:%S')
 
 # promote to active
-cp -r ./draft/* -t ./active/
+shopt -s extglob
+cp -r ./draft/!(*.example.*) -t ./active/
 sed -i "1s|^|# ${dat} \n|" ./active/plan.md
 
 # save in archive
-cp -r ./draft/ ./archive/${dat}
+mkdir ./archive/${dat}
+cp -r ./draft/!(*.example.*) ./archive/${dat}
